e8efa14d9705b2d2118bf641499a8c9dd139a8b2
kokushibo-rt <kokushibou.rt@gmail.com>
2025-03-15 11:19:20 +0500

    lexer finished as plan.
    no oct, hex and bin literals. no escape symbols. no multiline comments
    
    ast started

---
+++ compiler/include/lexer.h
@@ -0,0 +1,17 @@
+#ifndef SELENA_LEXER_H_
+#define SELENA_LEXER_H_
+
+// 09.03.2025
+
+#include <stack>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+#endif // SELENA_LEXER_H_
